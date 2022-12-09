7be163c66ce31fcb5406f945afdd7bb754c8153a
Peppa <810921727@qq.com>
2022-12-10 01:30:43 +0800

    feat: umi

--- config/routes.ts
+++ config/routes.ts
@@ -1,7 +1,7 @@
 const routes = [
   {
     path: '/',
-    component: '@/pages/index',
+    component: '@/pages/Home',
   },
 ];
 export default routes;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,21 @@
 import styles from './index.less';
+import { Button, Drawer } from 'antd';
+import { useBoolean } from 'ahooks';
+// @refresh reset
 const Home = () => {
+  const [open, { toggle }] = useBoolean(false);
+
   return (
     <div>
-      <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>Page index123</h1>
+      <Button onClick={toggle} type="primary">
+        Button
+      </Button>
+      <Drawer open={open} onClose={toggle} title="Basic Drawer" width={600}>
+        <div>
+          <p>this is drawer</p>
+        </div>
+      </Drawer>
     </div>
   );
 };
