94bc9777908e669bb31b54c944a8fbcbf228e8c9
Peppa <810921727@qq.com>
2023-03-05 20:05:06 +0800

    github actions

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 export default defineConfig({
   antd: {},
   routes: routes,
+  outputPath: './docs',
   mfsu: {},
   fastRefresh: true,
-  publicPath: "/",
+  publicPath: '/',
 });
