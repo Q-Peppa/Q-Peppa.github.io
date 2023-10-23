e69e6c6e2bc4001d4e5c8f52b6ed418e7edc53bf
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2023-10-23 22:36:28 +0800

    feat(core) : 添加大小和主题改变

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
-import { Button, message } from 'antd';
+import { Button, Divider, message, Select, Typography } from 'antd';
 import Editor, { useMonaco } from '@monaco-editor/react';
 
 function App() {
   const monaco = useMonaco();
   const valRef = React.useRef<string>();
+  const [theme, setTheme] = React.useState<string>('vs-dark');
+  const [font, setFont] = React.useState<number>(26);
   useEffect(() => {
     if (monaco) {
-      console.log(`monaco`, monaco)
+      console.log(`monaco`, monaco);
     }
-  }, [monaco])
+  }, [monaco]);
   const handleEditorChange = (value: string) => {
     valRef.current = value;
-  }
+  };
   const showValue = () => {
     // copy to clipboard
     const v = valRef.current;
@@ -23,22 +25,75 @@ function App() {
     document.execCommand('copy');
     document.body.removeChild(input);
     message.success('复制成功');
-  }
-  const Main = <Editor
-    onChange={ handleEditorChange }
-    theme="vs-dark"
-    height="90vh" defaultLanguage="javascript" />;
-  return <div
-    style={ {
-      width: 800,
-      border: "1px solid #000",
-    } }
-
-  >
-    <h3>LeetCode Hint</h3>
-    { Main }
-    <hr />
-    <Button onClick={ showValue }>复制代码</Button>
-  </div>
+  };
+  const handleSelectTheme = (value: string) => {
+    setTheme(value);
+  };
+  const handleSelectFont = (value: number) => {
+    setFont(font + value);
+  };
+  const Main = (
+    <Editor
+      options={{
+        fontSize: font,
+        minimap: {
+          enabled: false,
+        },
+        scrollBeyondLastLine: false,
+      }}
+      onChange={handleEditorChange}
+      theme={theme}
+      height="90vh"
+      defaultLanguage="javascript"
+    />
+  );
+  return (
+    <div
+      style={{
+        width: '80%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        gap: 10,
+      }}
+    >
+      <section
+        style={{
+          width: '100%',
+        }}
+      >
+        <Typography.Title>LeetCode Hint</Typography.Title>
+        <Divider />
+        {Main}
+      </section>
+      <aside>
+        <Button type={'primary'} onClick={showValue}>
+          复制代码
+        </Button>
+        <p className={'my-2'}>选择主题 </p>
+        <Select
+          onSelect={handleSelectTheme}
+          placeholder="please select theme"
+          options={[
+            {
+              label: 'vs-dark',
+              value: 'vs-dark',
+            },
+            {
+              label: 'vs-light',
+              value: 'vs-light',
+            },
+            {
+              label: 'hc-black',
+              value: 'hc-black',
+            },
+          ]}
+        />
+        <p className={'my-2'}>选择字体大小 </p>
+        <Button onClick={() => handleSelectFont(1)}> + </Button>
+        <span>{font}</span>
+        <Button onClick={() => handleSelectFont(-1)}> - </Button>
+      </aside>
+    </div>
+  );
 }
 export default App;
