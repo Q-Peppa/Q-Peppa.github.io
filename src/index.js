c59eb4bddc5a16bfda98a0fb6241ced4d8606428
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2024-10-06 11:11:02 +0800

    feat: 添加疲劳计算器

--- src/index.js
+++ src/index.js
@@ -17,10 +17,36 @@ if (con) {
             </label>
             <div class="flex">
               <span x-text="showMsg" x-ref="msg"></span>
-              <button type="button" @click="copyWithin($refs.msg)" :class="btnClass" x-text="copy" x-transition.duration.333ms></button>
+              <button type="button"
+              @click="copyWithin($refs.msg)"
+              :class="btnClass"
+              x-text="copy"
+              x-transition.duration.300ms
+              ></button>
+            </div>
+            <h1>疲劳计算器</h1>
+            <h2>模式1， 起始疲劳 到 终止疲劳</h2>
+            <div>
+              <label for="fatigueStart">
+                起始疲劳: <input type="text" placeholder="起始疲劳" id="fatigueStart" x-model="fatigue.start">
+              </label>
+              <label for="fatigueEnd">
+                终止疲劳: <input type="text" placeholder="终止疲劳" id="fatigueEnd" x-model="fatigue.end">
+              </label>
+              <h3 x-html="fatigue.calcTotal()"></h3>
+            </div>
+            <h2>模式2， 起始疲劳 加 疲劳次数</h2>
+            <div>
+              <label for="fatigueStart2">
+                起始疲劳: <input type="text" placeholder="起始疲劳" id="fatigueStart2" x-model="fatigue.startFatigue">
+              </label>
+               <label for="fatigueTimes">
+                疲劳次数: <input type="text" placeholder="疲劳次数" id="fatigueTimes" x-model="fatigue.times">
+              </label>
+               <h3 x-html="fatigue.calcTotal2()"></h3>
             </div>
             <hr>
           `
-  root.innerHTML = str
+  root.innerHTML = str.trim()
 }
 
--- src/store.js
+++ src/store.js
@@ -20,7 +20,33 @@ function initStore(){
         setTimeout(()=>{
           this.copy = 'Copy'
         }, 1000)
-      })
+        })
+      },
+      fatigue: {
+        start : 0,
+        end: 0 ,
+        startFatigue:0,
+        times:0,
+        calcTotal(){
+          if (!this.start || !this.end) return "输入点东西看看吧~"
+          if (this.start > this.end) return  "输入的疲劳好像不对~"
+          let total = 0;
+          for (let i=this.start ; i<=this.end; i++) {
+          total+= Number(i)
+        }
+        return `总共的疲劳为：  <span style="color: red">${total}</span>`
+      },
+        calcTotal2(){
+          const t = Number(this.times) || 0
+          const f = Number(this.startFatigue) || 0
+          if ( t > 200) return  "正无穷"
+          if (!f ) return "输入点东西看看吧~"
+          let total = 0
+          for (let i=0 ; i< t ; i++) {
+            total += Number(f) + i
+          }
+          return `总共的疲劳为：  <span style="color: red">${total}</span>`
+        }
     }
   }))
 }
