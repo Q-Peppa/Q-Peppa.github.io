1846dc849ffc8d56edeb9f75fd5426b8c9a54301
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2025-03-20 13:58:29 +0800

    use markdown file

--- src/app/core/footer/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, input} from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
-})
-export class FooterComponent {
-}
